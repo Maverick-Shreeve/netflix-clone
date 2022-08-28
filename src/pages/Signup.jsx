1a498f9d06d209af0caeec0cb339d890a9fefcf1
Maverick Shreeve <maverickshreeve@gmail.com>
2022-08-27 20:10:32 -0700

    onncecting firebase db to app

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
+
+const Signup = () => {
+  const [email, setEmail] = useState(''); // empty strings to take the values the user enters
+  const [password, setPassword] = useState('');
+  const { user, signUp } = UserAuth();
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {  //pass in the e so the form isnt submitted, prevents whole page from being submitted
+    e.preventDefault();
+    try {
+      await signUp(email, password);
+      navigate('/')
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <div className='w-full h-screen'>
+        <img
+          className=' absolute w-full h-full object-cover' // link below is direct bg image from netflix
+          src='https://assets.nflxext.com/ffe/siteui/vlv3/ac824598-245b-4828-b14f-5cff9074f4d0/2265eafd-f6db-443e-8fcd-70a0bf78c406/US-en-20220822-popsignuptwoweeks-perspective_alpha_website_small.jpg'
+          alt='/'
+        />
+        <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
+        <div className='fixed w-full px-4 py-24 z-50'>
+          <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
+            <div className='max-w-[320px] mx-auto py-16'>
+              <h1 className='text-3xl font-bold'>Sign Up</h1>
+              <form
+                onSubmit={handleSubmit}
+                className='w-full flex flex-col py-4'
+              >
+                <input
+                  onChange={(e) => setEmail(e.target.value)} // e is for the event of email and password being set
+                  className='p-3 my-2 bg-gray-700 rouded'
+                  type='email'
+                  placeholder='Email'
+                  autoComplete='email'
+                />
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  className='p-3 my-2 bg-gray-700 rouded'
+                  type='password'
+                  placeholder='Password'
+                  autoComplete='current-password'
+                />
+                <button className='bg-red-600 py-3 my-6 rounded font-bold'>
+                  Sign Up
+                </button>
+                <div className='flex justify-between items-center text-sm text-gray-600'>
+                  <p>
+                    <input className='mr-2' type='checkbox' />
+                    Remember me?
+                  </p>
+                  <p>Need Help??</p>
+                </div>
+                <p className='py-8'>
+                  <span className='text-red-600'>
+                    Already subbed to Netflix??
+                  </span>{' '}
+                  <Link to='/login'>Sign In Here</Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
